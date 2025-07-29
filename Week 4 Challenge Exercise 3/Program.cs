6c438611d5c5b58d2a19fc88227e319f9b3ed1bd
ElijahA1 <elijah.abourezk@outlook.com>
2025-07-29 14:41:52 -0700

    Add project files.

---
+++ Week 4 Challenge Exercise 3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week_4_Challenge_Exercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myString = [ 'h', 'e', 'l', 'l', 'o' ];
+            reverseString(myString);
+        }
+        static void reverseString(char[] myString) 
+        {
+            int counter = 0;
+
+            for (int i = myString.Length -1 ; i > myString.Length/2; i--) 
+            {
+                char first = myString[counter];
+                char last = myString[i];
+                myString[i] = first;
+                myString[counter] = last;
+
+                counter++;
+            }
+            Console.WriteLine();
+            foreach (char c in myString) {Console.Write(c); }
+        }
+    }
+}
